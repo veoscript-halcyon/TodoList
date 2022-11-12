9a781461468f891573b9970d4791e8c1857b8ca3
Jerome Villaruel <jeromevillaruel1998@gmail.com>
2022-11-12 13:44:16 +0800

    feat: add create todo functionality for redux saga (#1)
    Co-authored-by: name <jeromevillaruel1998@gmail.com

--- src/component/AddTodoModal.tsx
+++ src/component/AddTodoModal.tsx
@@ -3,22 +3,47 @@ import tw from 'twrnc'
 import { fonts } from '../styles/global'
 import { FeatherIcon } from '../config/Icons'
 import { View, Text, TextInput, TouchableOpacity, Modal } from 'react-native'
+import { CREATE_TODOS_REQUEST, SET_TODO_CONTENT, SET_TODO_TITLE } from '../redux/todos/action-types'
+import { connect } from 'react-redux'
 
 interface AddTodoModalPropsTypes {
+  todos: any
+  dispatch: any
+  refresh: any
   modalVisible: boolean
   handleCloseAddTodoModal: any
 }
 
-class AddTodoModal extends React.PureComponent<AddTodoModalPropsTypes, any> {
+class AddTodoModal extends React.PureComponent<AddTodoModalPropsTypes> {
 
   constructor(props: AddTodoModalPropsTypes) {
     super(props)
     this.state = {}
   }
 
+  handleCreateTodo = () => {
+    const { todos, dispatch, refresh, handleCloseAddTodoModal } = this.props
+    const { title, content } = todos
+
+    dispatch({
+      type: CREATE_TODOS_REQUEST,
+      payload: {
+        title,
+        content
+      }
+    })
+
+    refresh()
+    handleCloseAddTodoModal()
+  }
+
   render() {
+
+    const { dispatch } = this.props
+    
     return (
       <Modal
+        statusBarTranslucent={true}
         animationType="fade"
         transparent={true}
         visible={this.props.modalVisible}
@@ -45,15 +70,53 @@ class AddTodoModal extends React.PureComponent<AddTodoModalPropsTypes, any> {
                   />
                 </TouchableOpacity>
               </View>
-              <View style={tw`flex-col w-full my-5`}>
-                <Text style={[tw`text-sm text-neutral-500 ml-2 mb-2`, fonts.fontRaleway]}>Title</Text>
-                <TextInput
-                  style={[tw`px-3 py-1 w-full rounded-lg text-neutral-500 bg-white`, fonts.fontRaleway]}
-                />
+              <View style={tw`flex-col items-center w-full my-3`}>
+                <View style={tw`flex-col w-full`}>
+                  <Text style={[tw`text-sm text-neutral-500 ml-2 mb-2`, fonts.fontRaleway]}>Title</Text>
+                  <TextInput
+                    style={[tw`px-3 py-2 w-full rounded-lg text-neutral-600 border border-orange-400`, fonts.fontRaleway]}
+                    onChangeText={(text: string) => {
+                      dispatch({
+                        type: SET_TODO_TITLE,
+                        payload: {
+                          title: text
+                        }
+                      })
+                    }}
+                  />
+                </View>
+                <View style={tw`flex-col w-full my-2`}>
+                  <Text style={[tw`text-sm text-neutral-500 ml-2 mb-2`, fonts.fontRaleway]}>Content</Text>
+                  <TextInput
+                    multiline={true}
+                    numberOfLines={1}
+                    style={[tw`flex-row items-start px-3 py-2 w-full rounded-lg text-neutral-600 border border-orange-400`, fonts.fontRaleway]}
+                    onChangeText={(text: string) => {
+                      dispatch({
+                        type: SET_TODO_CONTENT,
+                        payload: {
+                          content: text
+                        }
+                      })
+                    }}
+                  />
+                </View>
               </View>
               <View style={tw`flex-row items-center justify-center w-full`}>
-                <Text>Gago</Text>
-                <Text>Gago</Text>
+                <TouchableOpacity
+                  activeOpacity={0.7}
+                  style={tw`flex-row items-center justify-center w-[5rem] mr-1 px-3 py-3 rounded-lg bg-neutral-400`}
+                  onPress={this.props.handleCloseAddTodoModal}
+                >
+                  <Text style={[tw`text-base text-white -mt-1`, fonts.fontRaleway]}>Cancel</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  activeOpacity={0.7}
+                  style={tw`flex-row items-center justify-center w-[5rem] px-3 py-3 rounded-lg bg-orange-400`}
+                  onPress={this.handleCreateTodo}
+                >
+                  <Text style={[tw`text-base text-white -mt-1`, fonts.fontRaleway]}>Save</Text>
+                </TouchableOpacity>
               </View>
             </View>
           </View>
@@ -63,4 +126,8 @@ class AddTodoModal extends React.PureComponent<AddTodoModalPropsTypes, any> {
   }
 }
 
-export default AddTodoModal
+const mapStateToProps = (state: any) => ({
+  todos: state.todosReducer
+})
+
+export default connect(mapStateToProps)(AddTodoModal)
--- src/redux/todos/api.ts
+++ src/redux/todos/api.ts
@@ -2,7 +2,19 @@ import api from "../../helpers/Axios";
 
 export const fetchTodos = async () => {
 	try {
-		const response = await api.get(`/api/get-todos`)
+		const response = await api.get('/api/todos')
+  	return response
+	} catch (error) {
+		console.error('fetch todos api error', error)
+	}
+}
+
+export const storeTodos = async (_args: { title: string, content: string }) => {
+	try {
+		const response = await api.post('/api/create-todo', {
+			title: _args.title,
+			content: _args.content 
+		})
   	return response
 	} catch (error) {
 		console.error('fetch todos api error', error)
